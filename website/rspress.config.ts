798bfb0b6407f75bca18a0c448fadd64c4f42b22
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2025-01-26 13:22:15 +0100

    update website

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -6,11 +6,13 @@ export default defineConfig({
 	root: path.join(__dirname, "docs"),
 	plugins: [rspressPluginLiveDemo()],
 
-	title: "My Site",
-	icon: "/rspress-icon.png",
+	title: "Live Demo",
+	icon: "/icon-dark.png",
+
+	logoText: "Live Demo",
 	logo: {
-		light: "/rspress-light-logo.png",
-		dark: "/rspress-dark-logo.png",
+		light: "/icon-light.svg",
+		dark: "/icon-dark.svg",
 	},
 
 	themeConfig: {
@@ -18,8 +20,7 @@ export default defineConfig({
 			{
 				icon: "github",
 				mode: "link",
-				// TODO: Replace the link
-				content: "https://github.com/web-infra-dev/rspress",
+				content: "https://github.com/ViktorZhurbin/live-demo",
 			},
 		],
 	},
@@ -29,14 +30,6 @@ export default defineConfig({
 	},
 
 	builderConfig: {
-		output: {
-			cssModules: {
-				localIdentName:
-					process.env.NODE_ENV === "development"
-						? "[folder]__[local]-[hash:base64:6]"
-						: "[local]-[hash:base64:6]",
-			},
-		},
 		performance: process.env.BUNDLE_ANALYZE
 			? {
 					bundleAnalyze: {
