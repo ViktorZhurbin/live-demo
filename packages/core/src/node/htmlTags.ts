7088f557e17d87d9255992714577ba9041a1def8
Viktor Zhurbin <vzhurbin@gmail.com>
2025-08-18 21:31:32 +0200

    up babel and rollup

--- packages/core/src/node/htmlTags.ts
+++ packages/core/src/node/htmlTags.ts
@@ -4,16 +4,14 @@ export const htmlTags = [
     tag: "script",
     head: true,
     attrs: {
-      src: "https://cdn.jsdelivr.net/npm/@babel/standalone@7.26.4/babel.min.js",
-      integrity: "sha256-oShy6o2j0psqKWxRv6x8SC6BQZx1XyIHpJrZt3IA9Oo=",
-      crossorigin: "anonymous",
+      src: "https://cdn.jsdelivr.net/npm/@babel/standalone@7.28.3/babel.min.js",
     },
   },
   {
     tag: "script",
     head: true,
     attrs: {
-      src: "https://cdn.jsdelivr.net/npm/@rollup/browser@4.31.0/dist/rollup.browser.min.js",
+      src: "https://cdn.jsdelivr.net/npm/@rollup/browser@4.46.3/dist/rollup.browser.min.js",
     },
   },
 ];
