458d308da434f562776c380d99070b1842d9c121
Viktor Zhurbin <vzhurbin@gmail.com>
2025-08-24 09:44:17 +0200

    cleanup

--- packages/plugin-docusaurus-test/src/remarkPlugin.ts
+++ packages/plugin-docusaurus-test/src/remarkPlugin.ts
@@ -1,4 +1,3 @@
-import fs from "fs";
 import type { Root } from "mdast";
 import type { MdxJsxFlowElement } from "mdast-util-mdx";
 import path from "path";
@@ -17,118 +16,112 @@ const CODE_BLOCK_NAME = "CodeBlock";
 const defaultModules = ["react"];
 
 export const remarkPlugin: Plugin<[], Root> = () => {
-  const uniqueImports = new Set(defaultModules);
+	const uniqueImports = new Set(defaultModules);
 
-  return (tree, vfile) => {
-    if (vfile.extname !== ".mdx" && vfile.extname !== ".md") return;
-    /** 1. Transorm:
-     * ```jsx live
-     *    const a = 1 + 3;
-     * ```
-     * into:
-     * <CodeBlock>
-     *    const a = 1 + 3;
-     * </CodeBlock>
-     */
-    visit(tree, "code", (node) => {
-      if (!node?.lang) return;
+	return (tree, vfile) => {
+		if (vfile.extname !== ".mdx" && vfile.extname !== ".md") return;
+		/** 1. Transorm:
+		 * ```jsx live
+		 *    const a = 1 + 3;
+		 * ```
+		 * into:
+		 * <CodeBlock>
+		 *    const a = 1 + 3;
+		 * </CodeBlock>
+		 */
+		visit(tree, "code", (node) => {
+			if (!node?.lang) return;
 
-      const isLive = node.meta?.includes("live");
+			const isLive = node.meta?.includes("live");
 
-      if (!(isLive && node.lang in LiveDemoLanguage)) return;
+			if (!(isLive && node.lang in LiveDemoLanguage)) return;
 
-      Object.assign(node, {
-        type: "mdxJsxFlowElement",
-        name: CODE_BLOCK_NAME,
-        children: [
-          {
-            type: "text",
-            value: node.value,
-          },
-        ],
-      });
+			Object.assign(node, {
+				type: "mdxJsxFlowElement",
+				name: CODE_BLOCK_NAME,
+				children: [
+					{
+						type: "text",
+						value: node.value,
+					},
+				],
+			});
 
-      return;
-    });
+			return;
+		});
 
-    /** 2. Transform:
-     * <CodeBlock src="../snippets/Component.tsx" />
-     *
-     * into:
-     *
-     * <CodeBlock>
-     *    const a = 1 + 3;
-     * </CodeBlock>
-     */
-    visit(tree, "mdxJsxFlowElement", (node: MdxJsxFlowElement) => {
-      if (node.name !== CODE_BLOCK_NAME) return;
+		/** 2. Transform:
+		 * <CodeBlock src="../snippets/Component.tsx" />
+		 *
+		 * into:
+		 *
+		 * <CodeBlock>
+		 *    const a = 1 + 3;
+		 * </CodeBlock>
+		 */
+		visit(tree, "mdxJsxFlowElement", (node: MdxJsxFlowElement) => {
+			if (node.name !== CODE_BLOCK_NAME) return;
 
-      const importPath = getMdxJsxAttribute(node, "src");
+			const importPath = getMdxJsxAttribute(node, "src");
 
-      if (typeof importPath !== "string") return;
+			if (typeof importPath !== "string") return;
 
-      const absolutePath = getAbsolutePath(importPath, vfile);
+			const absolutePath = getAbsolutePath(importPath, vfile);
 
-      const entryFile = resolveFileInfo({
-        importPath,
-        absolutePath,
-      });
+			const entryFile = resolveFileInfo({
+				importPath,
+				absolutePath,
+			});
 
-      const demo = getFilesAndImports({
-        uniqueImports,
-        ...entryFile,
-      });
+			const demo = getFilesAndImports({
+				uniqueImports,
+				...entryFile,
+			});
 
-      const props = {
-        files: demo.files,
-        entryFileName: entryFile.fileName,
-      };
+			const props = {
+				files: demo.files,
+				entryFileName: entryFile.fileName,
+			};
 
-      const code = fs.readFileSync(absolutePath, {
-        encoding: "utf8",
-      });
+			const attributes = getJsxAttributesFromProps(props);
 
-      const attributes = getJsxAttributesFromProps(props);
-
-      console.log({ attributes });
-
-      Object.assign(node, {
-        type: "mdxJsxFlowElement",
-        name: CODE_BLOCK_NAME,
-        attributes,
-        // children: [
-        //   {
-        //     type: "text",
-        //     value: code,
-        //   },
-        // ],
-      });
-    });
-  };
+			Object.assign(node, {
+				type: "mdxJsxFlowElement",
+				name: CODE_BLOCK_NAME,
+				attributes,
+				// children: [
+				//   {
+				//     type: "text",
+				//     value: code,
+				//   },
+				// ],
+			});
+		});
+	};
 };
 
 function getAbsolutePath(importPath: string, vfile: VFile) {
-  // Resolve relative to the current MDX file location
-  const mdxFileDir = path.dirname(vfile.path || vfile.history[0] || "");
-  const absolutePath = path.resolve(mdxFileDir, importPath);
+	// Resolve relative to the current MDX file location
+	const mdxFileDir = path.dirname(vfile.path || vfile.history[0] || "");
+	const absolutePath = path.resolve(mdxFileDir, importPath);
 
-  return absolutePath;
+	return absolutePath;
 }
 
 function getMdxJsxAttribute(node: MdxJsxFlowElement, attrName: string) {
-  const attribute = node.attributes?.find((attr) => {
-    return attr.type === "mdxJsxAttribute" && attr.name === attrName;
-  });
+	const attribute = node.attributes?.find((attr) => {
+		return attr.type === "mdxJsxAttribute" && attr.name === attrName;
+	});
 
-  return attribute?.value;
+	return attribute?.value;
 }
 
 function getJsxAttributesFromProps(
-  props: Record<string, unknown>,
+	props: Record<string, unknown>
 ): MdxJsxFlowElement["attributes"] {
-  return Object.entries(props).map(([name, value]) => ({
-    name,
-    value: JSON.stringify(value),
-    type: "mdxJsxAttribute",
-  }));
+	return Object.entries(props).map(([name, value]) => ({
+		name,
+		value: JSON.stringify(value),
+		type: "mdxJsxAttribute",
+	}));
 }
--- website-docusaurus/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/viktor/code/live-demo/website-docusaurus/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/viktor/code/live-demo/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/home/viktor/code/live-demo/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@docusaurus+faster@3.8.1_@docusaurus+types@3.8.1_@swc+c_c46c0c0c10b895f2579fcd3f2df722db/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/viktor/code/live-demo/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@docusaurus+faster@3.8.1_@docusaurus+types@3.8.1_@swc+c_c46c0c0c10b895f2579fcd3f2df722db/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/viktor/code/live-demo/website-docusaurus/src/css/custom.css"),
-  require("/home/viktor/code/live-demo/packages/plugin-docusaurus-test/dist/client.mjs"),
-];
